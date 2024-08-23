5a13a47e3b067b6ad1f25e9844efe8c3c1139bb9
_nicorette_craire <200042365@student.kangan.edu.au>
2024-08-23 14:43:17 +1000

    you did this in one day you absolute tech baddie_420

--- dice.py
+++ dice.py
@@ -1,1 +1,8 @@
-# put your dice_roll() function in this file
+
+import random   #   mod for generating random numbers
+
+def diceroll(faces, dices=1):   #parameter faces is the number of sides to the dice
+    """Simulate rolling multiple dice with the given number of faces."""
+    rolls = [random.randint(1, faces) for _ in range(dices)]
+    #  generates a random number between 1 & faces
+    return rolls   #  returns list of results
--- main.py
+++ main.py
@@ -1,24 +1,29 @@
 from pyscript import document  # webpage module links
 # your roll_dice function should be saved in a file named 'dice.py'
 # uncomment the next line when you have this prepared
-# import dice
-
+import dice
+import random
 
 # GLOBAL (script-wide) variable
 # this stores the selected face option from the drop-down list
-dice_type = "Coin"
+dice_type = "6"
 
 
 def select_face_option(event):
-    global dice_type  # use global var named dice_type
-    ...  # replace with your own code
+    global dice_type
+    dice_type = document.getElementById("dice-selector").value
+    print(f"Selected dice type: {dice_type}")
 
 
 def roll_all_dice(event):
-    global dice_type  # use global var named dice_type
-    ...  # replace with your own code
-
+    global dice_type
+    faces = int(dice_type)
+    num_dice = int(document.getElementById("num-dice").value)
+    rolls = dice.diceroll(faces, num_dice)
+    roll_history = document.getElementById("roll-history")
+    roll_history.innerHTML += f"<p>You rolled a {faces} faced dice {num_dice} times: {', '.join(map(str, rolls))}</p>"
 
 def clear_history(event):
     # this finds the div tag with id attribute 'roll-history' and clears whatever is inside
-    document.querySelector("div#roll-history").innerHTML = ""
+    document.querySelector("div#roll-history").innerHTML = "would you like to roll again?"
+
